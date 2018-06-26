20f81afef732a7425c1caeb18e72f2b179fea45c
Michael Rauch <git.mra@miranet.ch>
2018-06-26 13:38:41 +0200

    fetcher: initial contribution

---
+++ fetcher/index.js
@@ -0,0 +1,32 @@
+// start silent: npm start -s
+
+const fetch = require('node-fetch');
+
+const fetchUrl = 'http://stat.integ.stadt-zuerich.ch/tags/';
+// const fetchUrl = 'http://stat.integ.stadt-zuerich.ch/static/touch-icon.png';
+
+function log(url, status, duration, comment) {
+    var ts = new Date().toISOString();
+    console.log(`${ts};${url};${status};${duration};${comment != undefined ? comment : ''}`);
+}
+
+function elapsedTime(startMilllis) {
+    return Date.now() - startMilllis;
+}
+
+const startMilllis = Date.now();
+fetch(fetchUrl, {
+    method: 'GET',
+    headers: { 'Accept': 'application/json'}
+  })
+  .then(response => {
+    log(fetchUrl, response.status, elapsedTime(startMilllis));
+
+    if(response.ok) {
+      return response.text();
+    } 
+  }).then(json => {
+    // console.log(json);
+  }).catch(error => {
+    log(fetchUrl, -1, elapsedTime(startMilllis), `"fetch failed: ${error.message}"`);
+  });

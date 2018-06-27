25c3545916c46b4d10b51aedf83386016529b8af
Michael Rauch <git.mra@miranet.ch>
2018-06-27 20:50:17 +0200

    hint for find exec

--- fetcher/run_parametrized.sh
+++ fetcher/run_parametrized.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# usage: run_parametrized.sh < sample.args 
+# usage:
+#     run_parametrized.sh < sample.args
+
+# to run all *.args from shell (or crontab):
+#    find . -name "*.args" -exec sh -c "./run_parametrized.sh < {}" \;
 
 # SCRIPT_HOME=$(dirname -- $(readlink -fn -- "$0"))
 # https://stackoverflow.com/questions/5256823/pwd-command-with-shell-script
